e4cd43ff13ac2ee99bcaf95ed0612f1c74b0d624
chang-lab-mayo-clinic <153653312+chang-lab-mayo-clinic@users.noreply.github.com>
2023-12-14 17:49:58 -0600

    Figure 4 rep 022421
    raw fluorescence data processing

---
+++ IAV_H1N1_022421_raw/analyze data/PCR_multiplex_analyze_v1_2.m
@@ -0,0 +1,156 @@
+%graph multiplex PCR data for FAM and CY5 probes in drops
+%2-15-21 v1.2
+%Geoff Zath
+
+%2-24-21 burst size experiment
+
+clear; clc
+
+%% Inputs
+
+cycle = 40; %cycle data to graph
+
+%heatmap
+Nx = 50; %x bin
+Ny = 50; %y bin
+
+xmin = -0.1;
+xmax = 0.5;
+
+ymin = -0.1;
+ymax = 0.5;
+
+
+%% Load data
+
+%fluorescence data
+PCR_data = load('processed_delRn_detection_data_IAV_022421.mat');
+
+PCR_cycles = PCR_data.cycle;
+FAM_filter = PCR_data.delRn_FAM_FINAL;
+CY5_filter = PCR_data.delRn_CY5_FAM_FINAL;
+FAM = PCR_data.delRn_FAM;
+CY5 = PCR_data.delRn_CY5;
+
+cycle_loc = find(PCR_cycles == cycle);
+
+FAM_filter_graph = FAM_filter{cycle_loc};
+CY5_filter_graph = CY5_filter{cycle_loc};
+FAM_graph = FAM{cycle_loc};
+CY5_graph = CY5{cycle_loc};
+
+
+
+%% Process data
+
+
+
+
+
+%% Figures
+
+%colors
+map = colormap(linspecer);
+%map(1,:) = 1; %background white = 1 or black = 0
+
+
+%Filtered Cy5 vs FAM
+figure(1); clf(1)
+
+plot(FAM_filter_graph,CY5_filter_graph,'.k','markersize',5)
+
+xlabel('FAM \DeltaRn (a.u.)')
+ylabel('CY5 \DeltaRn (a.u.)')
+
+title(['Filtered data cycle ',num2str(cycle,'%2.0f')])
+
+box on
+
+axis([xmin xmax ymin ymax])
+
+set(gca,'fontsize',14,'linewidth',1,'xminortick','on','yminortick','on','ticklength',[0.015 1])
+
+
+
+
+
+
+%heatmap of filtered Cy5 vs FAM
+figure(2); clf(2)
+
+%Make 2D histogram
+x = linspace(xmin, xmax, Nx);
+y = linspace(ymin, ymax, Ny);
+[N_2D Xedge Yedge] = histcounts2(FAM_filter_graph,CY5_filter_graph,x,y);
+imagesc(x,y,N_2D')
+set(gca, 'XLim', x([1 end]), 'YLim', y([1 end]), 'YDir', 'normal');
+
+box on
+colormap(map)
+c = colorbar('eastoutside','fontsize',14);
+c.Label.String = 'Number of Drops';
+
+title(['Filtered data, Cycle ',num2str(cycle,'%2.0f')])
+xlabel('FAM \DeltaRn (a.u.)')
+ylabel('CY5 \DeltaRn (a.u.)')
+axis([xmin xmax ymin ymax])
+set(gca,'fontsize',14,'linewidth',1,'xminortick','on','yminortick','on','ticklength',[0.015 1])
+
+
+
+
+
+
+%All Cy5 vs FAM
+figure(3); clf(3)
+
+plot(FAM_graph,CY5_graph,'.k','markersize',5)
+
+xlabel('FAM \DeltaRn (a.u.)')
+ylabel('CY5 \DeltaRn (a.u.)')
+
+title(['All data, Cycle ',num2str(cycle,'%2.0f')])
+
+box on
+
+axis([xmin xmax ymin ymax])
+
+set(gca,'fontsize',14,'linewidth',1,'xminortick','on','yminortick','on','ticklength',[0.015 1])
+
+
+
+
+
+
+%heatmap of all Cy5 vs FAM
+figure(4); clf(4)
+
+%Make 2D histogram
+x = linspace(xmin, xmax, Nx);
+y = linspace(ymin, ymax, Ny);
+[N_2D Xedge Yedge] = histcounts2(FAM_graph,CY5_graph,x,y);
+imagesc(x,y,N_2D')
+set(gca, 'XLim', x([1 end]), 'YLim', y([1 end]), 'YDir', 'normal');
+
+box on
+colormap(map)
+c = colorbar('eastoutside','fontsize',14);
+c.Label.String = 'Number of Drops';
+
+title(['All data, cycle ',num2str(cycle,'%2.0f')])
+xlabel('FAM \DeltaRn (a.u.)')
+ylabel('CY5 \DeltaRn (a.u.)')
+axis([xmin xmax ymin ymax])
+set(gca,'fontsize',14,'linewidth',1,'xminortick','on','yminortick','on','ticklength',[0.015 1])
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ IAV_H1N1_022421_raw/process data/PCR_detection_process_multi_v7.m
@@ -0,0 +1,666 @@
+%Process PCR detection data for MOI 0.1 (H1N1) burst size IAV drops
+%3-1-21 v7 add filter for drop measurements over time
+%delRn data so data is linked
+%Geoff Zath
+
+%delRn normalization
+
+%IAV drops 2-24-21
+
+%%add split ratio to estimated positive drops
+
+
+clear; clc
+
+%% Inputs
+dataname = 'detection_data_IAV_022421.mat'; %rename me
+nbins_curve = 1001; %number of bins for histogram curves (N+1)
+nbins = 100; %number of bins for histograms
+
+Z = 3.392; %Z value for CI calc at cycle 1 (95% = 1.960, 99% = 2.576. 99.5% = 2.807, 99.9% = 3.392)
+C40_mgene_avg = 0.4314; %from std curve
+C40_mgene_std = 0.0256;
+T_C40 = C40_mgene_avg - C40_mgene_std;
+
+edges_CY5 = linspace(0.4,2,nbins);
+
+%FAM thresholds by drop #
+T_FAM = [1 1.5e4; 1 1.25e4; 1.15e3 1.05e4; 1 7e3; 1 2.1e3; 1 1.25e4];
+edges_FAM = linspace(0,0.3,nbins);
+
+%time inside thresholds
+T_time = [4.5 6.5; 4.5 6.5; 4.5 6.5; 4.5 6.5; 4.5 6.5; 4.5 6.5];
+%T_time = [0 10; 0 10; 0 10; 0 10; 0 10; 0 10; 0 10; 0 10]; %no filter
+edges_time = linspace(0,10,nbins);
+
+%ROX thresholds
+T_ROX = [0.5 0.8; 0.5 0.8; 0.5 0.8; 0.5 0.8; 0.5 0.8; 0.5 0.8; 0.5 0.8; 0.5 0.8];
+%T_ROX = [0 10; 0 10; 0 10; 0 10; 0 10; 0 10; 0 10; 0 10]; %no filter
+edges_ROX = linspace(0,1,nbins);
+
+%delRn (FAM) thresholds (final cleanup) (set min at <+1% at cycle 1, set max at std cycle 40 mean - std)
+T_delRn_FAM = [0.025 0.406; 0.025 0.406; 0.025 0.406; 0.025 0.406; 0.025 0.406; 0.025 0.6]; 
+%T_delRn = [-2 10; -2 10; -2 10; -2 10; -2 10; -2 10; -2 10; -2 10]; %no filter
+edges_delRn_FAM = linspace(-0.1,1,nbins);
+
+%delRn (CY5) thresholds (final cleanup)
+T_delRn_CY5 = [-0.25 0.25; -0.25 0.5; -0.25 0.5; -0.25 1; -0.25 1; -0.25 1.5; -0.25 1.5; -0.25 2]; 
+%T_delRn = [-2 10; -2 10; -2 10; -2 10; -2 10; -2 10; -2 10; -2 10]; %no filter
+edges_delRn_CY5 = linspace(-0.1,1,nbins);
+
+
+%PMT gain correction
+gain_correct = 'N'; %Y/N
+m = 4.8572; %PMT gain slope (log10 scale)
+b = 1.4367; %PMT gain intercept (log10 scale)
+G_v_FAM_measured = 0.66; %V, PMT GAIN for measurements
+G_v_FAM_base = 0.55; %V, PMT GAIN from standard curve
+G_FAM_measured = 10^(m*G_v_FAM_measured + b); %convert PMT gain voltage to true gain (measured gain)
+G_FAM_base = 10^(m*G_v_FAM_base + b); %convert PMT gain voltage to true gain (standard curve gain)
+
+G_v_ROX_measured = 0.54; %V, PMT GAIN for measurements
+G_v_ROX_base = 0.6; %V, PMT GAIN from standard curve
+G_ROX_measured = 10^(m*G_v_ROX_measured + b); %convert PMT gain voltage to true gain (measured gain)
+G_ROX_base = 10^(m*G_v_ROX_base + b); %convert PMT gain voltage to true gain (standard curve gain)
+
+%% Load Data
+
+%detection data
+DETECTION = load(dataname);
+FAM = DETECTION.FAM;
+ROX = DETECTION.ROX;
+CY5 = DETECTION.CY5;
+TIME = DETECTION.Time_Inside;
+cycle = DETECTION.Cycles;
+C = length(cycle); %number of cycles
+
+%% Post Processing
+
+if gain_correct == 'Y'
+    
+    ratio_gain_FAM(1:3) = G_FAM_base/G_FAM_measured;
+    ratio_gain_FAM(4:9) = 1;
+    
+    ratio_gain_ROX(1:3) = G_ROX_base/G_ROX_measured; %only 1 through 3 adjusted
+    ratio_gain_ROX(4:9) = 1; 
+    
+else
+    
+    ratio_gain_FAM(1:9) = 1;
+    ratio_gain_ROX(1:9) = 1;
+    
+end
+
+
+%filter data
+% for i = 1 : C
+%     
+%     %load temp array
+%     FAM_temp_1 = FAM{i};
+%     FAM_temp_1 = FAM_temp_1*ratio_gain_FAM(i); %correct for change in PMT gain
+%     FAM_PMT_correct{i} = FAM_temp_1;
+%     ROX_temp_1 = ROX{i};
+%     ROX_temp_1 = ROX_temp_1*ratio_gain_ROX(i);
+%     TIME_temp_1 = TIME{i};
+%     drop_count_unfiltered(i) = length(FAM_temp_1);
+%     
+% %sort drops based on time inside
+%     FAM_temp_2 = FAM_temp_1(TIME_temp_1 >= T_time(i,1));
+%     ROX_temp_2 = ROX_temp_1(TIME_temp_1 >= T_time(i,1));
+%     TIME_temp_2 = TIME_temp_1(TIME_temp_1 >= T_time(i,1));
+%     
+%     FAM_temp_3 = FAM_temp_2(TIME_temp_2 < T_time(i,2));
+%     ROX_temp_3 = ROX_temp_2(TIME_temp_2 < T_time(i,2));
+%     TIME_temp_3 = TIME_temp_2(TIME_temp_2 < T_time(i,2));
+%     
+%     %sort drops based on ROX
+% 	FAM_temp_4 = FAM_temp_3(ROX_temp_3 >= T_ROX(i,1));
+%     TIME_temp_4 = TIME_temp_3(ROX_temp_3 >= T_ROX(i,1));
+%     ROX_temp_4 = ROX_temp_3(ROX_temp_3 >= T_ROX(i,1));
+%     
+% 	FAM_temp_5 = FAM_temp_4(ROX_temp_4 < T_ROX(i,2));
+%     TIME_temp_5 = TIME_temp_4(ROX_temp_4 < T_ROX(i,2));
+%     ROX_temp_5 = ROX_temp_4(ROX_temp_4 < T_ROX(i,2));
+%       
+%     FAM_corr{i} = FAM_temp_5;
+%     FAM_avg(i) = mean(FAM_temp_5);
+%     FAM_std(i) = std(FAM_temp_5);
+%     
+%     ROX_corr{i} = ROX_temp_5;
+%     ROX_avg(i) = mean(ROX_temp_5);
+%     ROX_std(i) = std(ROX_temp_5);
+%     
+%     
+%     %Rn
+%     Rn{i} = FAM_temp_5./ROX_temp_5;
+%     Rn_avg(i) = mean(FAM_temp_5./ROX_temp_5);
+%     Rn_std(i) = std(FAM_temp_5./ROX_temp_5);
+%     
+%     
+%     %delRn
+%     delRn{i} = (FAM_temp_5./ROX_temp_5) - Rn_avg(1);
+%     delRn_avg(i) = mean((FAM_temp_5./ROX_temp_5) - Rn_avg(1));
+%     delRn_std(i) = std((FAM_temp_5./ROX_temp_5) - Rn_avg(1));
+%    
+%     %delRn/Rn0
+%     delRn_N{i} = ((FAM_temp_5./ROX_temp_5) - Rn_avg(1))/Rn_avg(1);
+%     delRn_N_avg(i) = mean(((FAM_temp_5./ROX_temp_5) - Rn_avg(1))/Rn_avg(1));
+%     delRn_N_std(i) = std(((FAM_temp_5./ROX_temp_5) - Rn_avg(1))/Rn_avg(1));
+%     
+%     %filter delRn
+%     delRn_temp_1 = delRn{i};
+%     
+%     delRn_temp_2 = delRn_temp_1(delRn_temp_1 > T_delRn_FAM(i,1));
+%     delRn_temp_3 = delRn_temp_2(delRn_temp_2 < T_delRn_FAM(i,2));
+%     
+%     delRn_filter{i} = delRn_temp_3;
+%     delRn_filter_avg(i) = mean(delRn_temp_3);
+%     delRn_filter_std(i) = std(delRn_temp_3);
+%     
+% end
+
+
+
+
+
+%filter data
+for i = 1 : C
+    
+    %load temp array
+    FAM_temp_1 = FAM{i};
+    FAM_temp_1 = FAM_temp_1*ratio_gain_FAM(i); %correct for change in PMT gain
+    FAM_PMT_correct{i} = FAM_temp_1;
+    CY5_temp_1 = CY5{i};
+    ROX_temp_1 = ROX{i};
+    ROX_temp_1 = ROX_temp_1*ratio_gain_ROX(i);
+    TIME_temp_1 = TIME{i};
+    drop_count_unfiltered(i) = length(FAM_temp_1);
+    
+    %sort drops based on drop#
+    FAM_temp_1 = FAM_temp_1(T_FAM(i,1):T_FAM(i,2));
+    CY5_temp_1 = CY5_temp_1(T_FAM(i,1):T_FAM(i,2));
+    ROX_temp_1 = ROX_temp_1(T_FAM(i,1):T_FAM(i,2));
+    TIME_temp_1 = TIME_temp_1(T_FAM(i,1):T_FAM(i,2));
+    
+    %sort drops based on time inside
+    FAM_temp_2 = FAM_temp_1(TIME_temp_1 >= T_time(i,1));
+    CY5_temp_2 = CY5_temp_1(TIME_temp_1 >= T_time(i,1));
+    ROX_temp_2 = ROX_temp_1(TIME_temp_1 >= T_time(i,1));
+    TIME_temp_2 = TIME_temp_1(TIME_temp_1 >= T_time(i,1));
+    
+    FAM_temp_3 = FAM_temp_2(TIME_temp_2 < T_time(i,2));
+    CY5_temp_3 = CY5_temp_2(TIME_temp_2 < T_time(i,2));
+    ROX_temp_3 = ROX_temp_2(TIME_temp_2 < T_time(i,2));
+    TIME_temp_3 = TIME_temp_2(TIME_temp_2 < T_time(i,2));
+    
+    %sort drops based on ROX
+	FAM_temp_4 = FAM_temp_3(ROX_temp_3 >= T_ROX(i,1));
+    CY5_temp_4 = CY5_temp_3(ROX_temp_3 >= T_ROX(i,1));
+    TIME_temp_4 = TIME_temp_3(ROX_temp_3 >= T_ROX(i,1));
+    ROX_temp_4 = ROX_temp_3(ROX_temp_3 >= T_ROX(i,1));
+    
+	FAM_temp_5 = FAM_temp_4(ROX_temp_4 < T_ROX(i,2));
+    CY5_temp_5 = CY5_temp_4(ROX_temp_4 < T_ROX(i,2));
+    TIME_temp_5 = TIME_temp_4(ROX_temp_4 < T_ROX(i,2));
+    ROX_temp_5 = ROX_temp_4(ROX_temp_4 < T_ROX(i,2));
+      
+    FAM_corr{i} = FAM_temp_5;
+    FAM_avg(i) = mean(FAM_temp_5);
+    FAM_std(i) = std(FAM_temp_5);
+    
+    CY5_corr{i} = CY5_temp_5;
+    CY5_avg(i) = mean(CY5_temp_5);
+    CY5_std(i) = std(CY5_temp_5);
+    
+    ROX_corr{i} = ROX_temp_5;
+    ROX_avg(i) = mean(ROX_temp_5);
+    ROX_std(i) = std(ROX_temp_5);
+    
+    
+    %Rn (FAM)
+    Rn_FAM{i} = FAM_temp_5./ROX_temp_5;
+    Rn_FAM_avg(i) = mean(FAM_temp_5./ROX_temp_5);
+    Rn_FAM_std(i) = std(FAM_temp_5./ROX_temp_5);
+    
+    
+    %delRn (FAM)
+    delRn_FAM{i} = (FAM_temp_5./ROX_temp_5) - Rn_FAM_avg(1);
+    delRn_FAM_avg(i) = mean((FAM_temp_5./ROX_temp_5) - Rn_FAM_avg(1));
+    delRn_FAM_std(i) = std((FAM_temp_5./ROX_temp_5) - Rn_FAM_avg(1));
+    
+    
+    %Rn (Cy5)
+    Rn_CY5{i} = CY5_temp_5./ROX_temp_5;
+    Rn_CY5_avg(i) = mean(CY5_temp_5./ROX_temp_5);
+    Rn_CY5_std(i) = std(CY5_temp_5./ROX_temp_5);
+    
+    
+    %delRn (Cy5)
+    delRn_CY5{i} = (CY5_temp_5./ROX_temp_5) - Rn_CY5_avg(1);
+    delRn_CY5_avg(i) = mean((CY5_temp_5./ROX_temp_5) - Rn_CY5_avg(1));
+    delRn_CY5_std(i) = std((CY5_temp_5./ROX_temp_5) - Rn_CY5_avg(1));
+    
+    
+    %filter delRn (FAM)
+    delRn_FAM_temp_1 = delRn_FAM{i};
+    
+    delRn_FAM_temp_2 = delRn_FAM_temp_1(delRn_FAM_temp_1 > T_delRn_FAM(i,1));
+    delRn_FAM_temp_3 = delRn_FAM_temp_2(delRn_FAM_temp_2 < T_delRn_FAM(i,2));
+    
+    delRn_FAM_filter{i} = delRn_FAM_temp_3;
+    delRn_FAM_filter_avg(i) = mean(delRn_FAM_temp_3);
+    delRn_FAM_filter_std(i) = std(delRn_FAM_temp_3);
+    
+    
+    %filter delRn (Cy5)
+    delRn_CY5_temp_1 = delRn_CY5{i};
+    
+    delRn_CY5_temp_2 = delRn_CY5_temp_1(delRn_CY5_temp_1 > T_delRn_CY5(i,1));
+    delRn_CY5_temp_3 = delRn_CY5_temp_2(delRn_CY5_temp_2 < T_delRn_CY5(i,2));
+    
+    delRn_CY5_filter{i} = delRn_CY5_temp_3;
+    delRn_CY5_filter_avg(i) = mean(delRn_CY5_temp_3);
+    delRn_CY5_filter_std(i) = std(delRn_CY5_temp_3);
+    
+    
+    %filter delRn (Cy5) by FAM
+    delRn_CY5_FAM_temp_1 = delRn_CY5{i};
+    
+    delRn_CY5_FAM_temp_2 = delRn_CY5_FAM_temp_1(delRn_FAM_temp_1 > T_delRn_FAM(i,1));
+    delRn_CY5_FAM_temp_3 = delRn_CY5_FAM_temp_2(delRn_FAM_temp_2 < T_delRn_FAM(i,2));
+    
+    delRn_CY5_FAM_filter{i} = delRn_CY5_FAM_temp_3;
+    delRn_CY5_FAM_filter_avg(i) = mean(delRn_CY5_temp_3);
+    delRn_CY5_FAM_filter_std(i) = std(delRn_CY5_temp_3);
+    
+    
+end
+
+delRn_FAM_posCI_C1 = delRn_FAM_avg(1) + Z*delRn_FAM_std(1)/sqrt(length(delRn_FAM{1}));
+
+
+
+%percent amplified
+for i = 1 : C;
+    
+    P_amp_FAM(i) = length(delRn_FAM_filter{i})/length(delRn_FAM{i})*100;
+    P_amp_CY5(i) = length(delRn_CY5_filter{i})/length(delRn_CY5{i})*100;
+    N_T(i) = length(delRn_FAM{i});
+    N_P(i) = length(delRn_FAM_filter{i});
+    
+end
+
+
+
+%save positive drops for all cycles
+for i = 1 : C
+    
+        
+    pos_FAM_temp = delRn_FAM_filter{i};
+    pos_CY5_temp = delRn_CY5_filter{i};
+    pos_CY5_FAM_temp = delRn_CY5_FAM_filter{i};
+        
+    %end
+    
+    %save positive drop threshold
+    delRn_FAM_FINAL{i} = pos_FAM_temp;
+    delRn_FAM_avg_FINAL(i) = mean(delRn_FAM_FINAL{i});
+    delRn_FAM_std_FINAL(i) = std(delRn_FAM_FINAL{i});
+    
+    delRn_CY5_FINAL{i} = pos_CY5_temp;
+    delRn_CY5_avg_FINAL(i) = mean(delRn_CY5_FINAL{i});
+    delRn_CY5_std_FINAL(i) = std(delRn_CY5_FINAL{i});
+    
+    delRn_CY5_FAM_FINAL{i} = pos_CY5_FAM_temp;
+
+end
+
+savename = ['processed_delRn_' dataname];
+
+save(savename,'delRn_FAM_avg_FINAL','delRn_FAM_std_FINAL','cycle','delRn_FAM_FINAL',...
+    'delRn_CY5_avg_FINAL','delRn_CY5_std_FINAL','delRn_CY5_FINAL','FAM_PMT_correct',...
+    'ROX','TIME','FAM_corr','ROX_corr','delRn_CY5_FAM_FINAL','delRn_FAM','delRn_CY5')
+
+
+
+
+ 
+%% Figures
+
+%colors
+red = linspecer('red');
+blue = linspecer('blue');
+red = linspecer('red');
+
+%subplot of detection delRn (FAM) histograms
+figure(1); clf(1)
+
+hold on
+
+for i = 1 : C
+    
+    subplot(C,1,i)
+    histogram(delRn_FAM{i},edges_delRn_FAM)
+    xline(T_delRn_FAM(i,1),'-k','linewidth',1);
+    xline(T_delRn_FAM(i,2),'-k','linewidth',1);
+    title(['Cycle ',num2str(cycle(i),'%2.0f')])
+    ylabel('Drop Count')
+    axis([min(edges_delRn_FAM) max(edges_delRn_FAM) 0 inf])
+    
+end
+
+xlabel('FAM \DeltaRn (a.u.)')
+suptitle('FAM delRn Detection Data')
+
+hold off
+
+
+
+
+
+%subplot of detection time inside histograms
+figure(2); clf(2)
+
+hold on
+
+
+for i = 1 : C
+    
+    subplot(C,1,i)
+    histogram(TIME{i},edges_time)
+    xline(T_time(i,1),'-k','linewidth',1);
+    xline(T_time(i,2),'-k','linewidth',1);
+    title(['Cycle ',num2str(cycle(i),'%2.0f')])
+    ylabel('Drop Count')
+    axis([min(edges_time) max(edges_time) 0 inf])
+    
+end
+
+xlabel('Time Inside (ms)')
+suptitle('Time Inside Detection Data')
+
+hold off
+
+
+
+
+
+%subplot of detection ROX histograms
+figure(3); clf(3)
+
+hold on
+
+
+for i = 1 : C
+    
+    subplot(C,1,i)
+    histogram(ROX{i}*ratio_gain_ROX(i),edges_ROX)
+    xline(T_ROX(i,1),'-k','linewidth',1);
+    xline(T_ROX(i,2),'-k','linewidth',1);
+    title(['Cycle ',num2str(cycle(i),'%2.0f')])
+    ylabel('Drop Count')
+    axis([min(edges_ROX) max(edges_ROX) 0 inf])
+    
+end
+
+xlabel('ROX Intensity (a.u.)')
+suptitle('ROX Detection Data')
+
+hold off
+
+
+
+
+
+%subplot of detection FAM histograms
+figure(4); clf(4)
+
+hold on
+
+
+for i = 1 : C
+    
+    subplot(C,1,i)
+    histogram(FAM_PMT_correct{i},edges_FAM)
+    title(['Cycle ',num2str(cycle(i),'%2.0f')])
+    ylabel('Drop Count')
+    axis([min(edges_FAM) max(edges_FAM) 0 inf])
+    
+end
+
+xlabel('FAM Intensity (a.u.)')
+suptitle('FAM Detection Data')
+
+hold off
+
+
+
+
+
+
+%subplot of filtered detection delRn (FAM) histograms
+figure(5); clf(5)
+
+hold on
+
+for i = 1 : C
+    
+    subplot(C,1,i)
+    histogram(delRn_FAM_filter{i},edges_delRn_FAM)
+    %xline(T_delRn(i,1),'-k','linewidth',1);
+    %xline(T_delRn(i,2),'-k','linewidth',1);
+    title(['Cycle ',num2str(cycle(i),'%2.0f')])
+    ylabel('Drop Count')
+    axis([min(edges_delRn_FAM) max(edges_delRn_FAM) 0 inf])
+    
+end
+
+xlabel('FAM \DeltaRn (a.u.)')
+suptitle('filtered FAM delRn Detection Data')
+
+hold off
+
+
+
+
+
+
+%subplot of detection CY5 histograms
+figure(6); clf(6)
+
+hold on
+
+
+for i = 1 : C
+    
+    subplot(C,1,i)
+    histogram(CY5{i},edges_CY5)
+    title(['Cycle ',num2str(cycle(i),'%2.0f')])
+    ylabel('Drop Count')
+    axis([min(edges_CY5) max(edges_CY5) 0 inf])
+    
+end
+
+xlabel('Cy5 Intensity (a.u.)')
+suptitle('CY5 Detection Data')
+
+hold off
+
+
+
+
+
+%subplot of detection delRn (Cy5) histograms
+figure(7); clf(7)
+
+hold on
+
+for i = 1 : C
+    
+    subplot(C,1,i)
+    histogram(delRn_CY5{i},edges_delRn_CY5)
+    %xline(T_delRn_CY5(i,1),'-k','linewidth',1);
+    %xline(T_delRn_CY5(i,2),'-k','linewidth',1);
+    title(['Cycle ',num2str(cycle(i),'%2.0f')])
+    ylabel('Drop Count')
+    axis([min(edges_delRn_CY5) max(edges_delRn_CY5) 0 inf])
+    
+end
+
+xlabel('CY5 \DeltaRn (a.u.)')
+%xlabel('Intensity (a.u.)')
+suptitle('CY5 delRn Detection Data')
+
+hold off
+
+
+
+
+
+%subplot of filtered detection delRn (Cy5) histograms
+figure(8); clf(8)
+
+hold on
+
+for i = 1 : C
+    
+    subplot(C,1,i)
+    histogram(delRn_CY5_FAM_filter{i},edges_delRn_CY5)
+    %xline(T_delRn(i,1),'-k','linewidth',1);
+    %xline(T_delRn(i,2),'-k','linewidth',1);
+    title(['Cycle ',num2str(cycle(i),'%2.0f')])
+    ylabel('Drop Count')
+    axis([min(edges_delRn_CY5) max(edges_delRn_CY5) 0 inf])
+    
+end
+
+xlabel('CY5 \DeltaRn (a.u.)')
+suptitle('filtered CY5 delRn Detection Data')
+
+hold off
+
+
+
+
+
+%subplot of detection time inside scatter plot
+figure(9); clf(9)
+
+hold on
+
+
+for i = 1 : C
+    
+    subplot(C,1,i)
+    plot(TIME{i},'.k','markersize',1)
+    yline(T_time(i,1),'-k','linewidth',1);
+    yline(T_time(i,2),'-k','linewidth',1);
+    xline(T_FAM(i,1),'-k','linewidth',1);
+    xline(T_FAM(i,2),'-k','linewidth',1);
+    title(['Cycle ',num2str(cycle(i),'%2.0f')])
+    ylabel('Time Inside (ms)')
+    
+    axis([0 inf min(edges_time) max(edges_time)])   
+end
+
+xlabel('Drop #')
+suptitle('Time Inside Detection Data')
+
+hold off
+
+
+
+
+
+%subplot of detection ROX scatter plot
+figure(10); clf(10)
+
+hold on
+
+
+for i = 1 : C
+    
+    subplot(C,1,i)
+    plot(ROX{i}*ratio_gain_ROX(i),'.k','markersize',1)
+    yline(T_ROX(i,1),'-k','linewidth',1);
+    yline(T_ROX(i,2),'-k','linewidth',1);
+    xline(T_FAM(i,1),'-k','linewidth',1);
+    xline(T_FAM(i,2),'-k','linewidth',1);
+    title(['Cycle ',num2str(cycle(i),'%2.0f')])
+    ylabel('ROX Intensity (a.u.)')
+    axis([0 inf min(edges_ROX) max(edges_ROX)])
+    
+end
+
+xlabel('Drop #')
+suptitle('ROX Detection Data')
+
+hold off
+
+
+
+
+
+%subplot of detection FAM scatter plot
+figure(11); clf(11)
+
+hold on
+
+
+for i = 1 : C
+    
+    subplot(C,1,i)
+    plot(FAM_PMT_correct{i},'.k','markersize',1)
+    xline(T_FAM(i,1),'-k','linewidth',1);
+    xline(T_FAM(i,2),'-k','linewidth',1);
+    title(['Cycle ',num2str(cycle(i),'%2.0f')])
+    ylabel('FAM Intensity (a.u.)')
+    axis([0 inf min(edges_FAM) max(edges_FAM)])
+    
+end
+
+xlabel('Drop #')
+suptitle('FAM Detection Data')
+
+hold off
+
+
+
+
+
+
+
+%subplot of detection CY5 scatter plot
+figure(12); clf(12)
+
+hold on
+
+
+for i = 1 : C
+    
+    subplot(C,1,i)
+    plot(CY5{i},'.k','markersize',1)
+    %xline(T_FAM(i,1),'-k','linewidth',1);
+    %xline(T_FAM(i,2),'-k','linewidth',1);
+    title(['Cycle ',num2str(cycle(i),'%2.0f')])
+    ylabel('CY5 Intensity (a.u.)')
+    axis([0 inf min(edges_CY5) max(edges_CY5)])
+    
+end
+
+xlabel('Drop #')
+suptitle('CY5 Detection Data')
+
+hold off
+
+
+
+
+
+
+
+
+
+
+
+
